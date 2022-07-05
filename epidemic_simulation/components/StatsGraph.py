a77c662c037646b0ddb554aaff3558c2d515c8c2
Isaias Rojas <s_isaiasr@tutanota.com>
2022-07-05 10:49:14 -0400

    add sir model

--- epidemic_simulation/components/StatsGraph.py
+++ epidemic_simulation/components/StatsGraph.py
@@ -1,17 +1,25 @@
 ##ADDING STATS
+import pygame
 from pygame.surface import Surface
-from epidemic_simulation.utils.constants import WIDTH, HEIGHT
+from epidemic_simulation.utils.constants import WIDTH, HEIGHT, GREY
 
 
-class StatsGraph(Surface):
+class StatsGraph:
 
-    def __init__(self):
-        super((WIDTH // 4, HEIGHT // 4))
+    def __init__(self, width, heigth, pos_x, pos_y, time):
+        self.width = width
+        self.heigth = heigth
+        self.stats = pygame.Surface(
+            (self.width, self.heigth)
+        )
+        self.pos = (pos_x, pos_y)
+        self.T = time
 
     def configure(self):
-        pass
+        self.stats.fill(GREY)
+        self.stats.set_alpha(230)
 
-    def configure_text(self):
+    def configure_text(self, susceptible, infected, recovered, dead):
         pass
 
     def update_graph(self):
@@ -19,3 +27,9 @@ class StatsGraph(Surface):
 
     def draw_graph(self):
         pass
+
+    def get_height(self):
+        return self.stats.get_height()
+
+    def get_width(self):
+        return self.stats.get_width()
